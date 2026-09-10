289e98d487e392a44adee634fb6643d64a5831e3
rishikesh singh <rishikeshsingh526@gmail.com>
2026-09-10 17:36:26 +0530

    3rd choice me avi thora fault hai aacha se nhi hua hai

---
+++ TO_DO_LIST/todo.py
@@ -0,0 +1,53 @@
+
+task = [] 
+
+while True:
+      
+    print("ENTER YOUR CHOICE")
+    print("1 Add Task")
+    print("2 View Task")
+    print("3 Delete Task")
+    print("4 Exit")
+
+    choice = int(input("Enter Your Choice (1-4) :"))
+
+    if choice == 1:
+        task_1 = input("Enter your task :")
+        task.append(task_1)
+        
+        print("task added succesfully.......")
+        print(task)
+
+
+    elif choice == 2:
+        if len(task) == 0:
+            print("No task available ")
+        else:
+            print("\n your task ")
+
+            for i, task in enumerate(task, start=1):
+                    print(f"{i}. {task}")
+
+    elif choice == 3:
+        if len(task) == 0:
+            print("No Task available...")
+
+        else: 
+            print("\n your task.")
+
+            for i, task in enumerate(task, start=1):
+                            print(f"{i}. {task}")
+
+        task_number = int(input("enter a task number to delete :"))
+
+        remove_task = task.pop(task_number - 1)
+
+        print(f"{remove_task} deleted..")
+
+
+    elif choice == 4:
+        print("goodbyeee")
+    
+
+    else:
+        print("invalidd")
